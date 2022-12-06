720be662e3a671b1eba2a6f7efd4c16a562036e9
Yousef Al-waer <elwaeryousef@gmail.com>
2022-12-06 18:20:15 +0200

    adding more indicators

--- cypress/e2e/SWE_PROJECT/PageObjects/ForgetObjects/ForgetPasswordObjects.cy.js
+++ cypress/e2e/SWE_PROJECT/PageObjects/ForgetObjects/ForgetPasswordObjects.cy.js
@@ -2,8 +2,8 @@
 class ForgetPasswordObjects {
     navigate() {
         cy.visit(Cypress.env("CYPRESSBASEURL")+'/login')
-        console.log(Cypress.env("CYPRESSBASEURL"), "waer")
-        cy.log(Cypress.env("CYPRESSBASEURL"), "waer")
+        console.log("waer" ,Cypress.env("CYPRESSBASEURL"), "waer")
+        cy.log( "waer" ,Cypress.env("CYPRESSBASEURL"), "waer")
     } 
     
     //----------------------------------------Test #1----------------------------------------// 
